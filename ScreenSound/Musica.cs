d6459ffbaae8165eae204e8ea592ed3688f63e30
Lucas Garcia <lucasugarcia@gmail.com>
2026-03-30 21:34:54 -0300

    Versão inicial com as classes Musica, Album e Genero

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,27 @@
+﻿class Musica
+{
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"{Nome} - {Artista}";
+    public Genero Genero { get; set; }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        
+        if (Disponivel)
+            Console.WriteLine($"Música disponível");
+        else
+            Console.WriteLine($"Adquira o plano");
+    }
+
+    public void ExibirNomeEArtista()
+    {
+        Console.WriteLine(DescricaoResumida);
+    }
+}
+
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,23 @@
+﻿Musica musica1 = new Musica();
+musica1.Artista = "Rodolfo Abrantes";
+musica1.Nome = "Sujo e Descalço";
+musica1.Duracao = 240;
+musica1.Disponivel = true;
+musica1.Genero = new Genero();
+musica1.Genero.Nome = "Rock";
+
+Musica musica2 = new Musica();
+musica2.Artista = "Rodolfo Abrantes";
+musica2.Nome = "Isaías 9";
+musica2.Duracao = 720;
+musica2.Disponivel = true;
+musica2.Genero = new Genero();
+musica2.Genero.Nome = "Gospel";
+
+Album album = new Album();
+album.Nome = "RABT";
+
+album.AdicionarMusica(musica1);
+album.AdicionarMusica(musica2);
+
+album.ExibirMusicasDoAlbum();
