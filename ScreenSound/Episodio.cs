735695138de2c8b46f243aa59922a2d6dbd5c453
Lucas Garcia <lucasugarcia@gmail.com>
2026-03-31 21:08:19 -0300

    Criação das classes Episodio e Podcast segundo o desafio proposto

---
+++ ScreenSound/Episodio.cs
@@ -0,0 +1,21 @@
+﻿class Episodio
+{
+    public int Duracao { get; }
+    public int Ordem { get; }
+    public string Titulo { get; }
+
+    private List<string> convidados = new List<string>();
+    public string Resumo => $"{Ordem} - {Titulo} ({Duracao} minutos) - {string.Join(", ", convidados)}";
+
+    public Episodio(int duracao, int ordem, string titulo)
+    {
+        Duracao = duracao;
+        Ordem = ordem;
+        Titulo = titulo;
+    }
+
+    public void AdicionarConvidado(string convidado)
+    {
+        convidados.Add(convidado);
+    }
+}
---
+++ ScreenSound/Podcast.cs
@@ -0,0 +1,29 @@
+﻿class Podcast
+{
+    public string Host { get; }
+    public string Nome { get; }
+
+    private List<Episodio> episodios = new List<Episodio>();
+    public int TotalEpisodios => episodios.Count;
+
+    public Podcast(string host, string nome)
+    {
+        Host = host;
+        Nome = nome;
+    }
+
+    public void AdicionarEpisodio(Episodio episodio)
+    {
+        episodios.Add(episodio);
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Podcast {Nome} - Host: {Host}");
+
+        foreach(var episodio in episodios.OrderBy(e => e.Ordem))
+            Console.WriteLine($"{episodio.Resumo}");
+
+        Console.WriteLine($"Total de episódios: {TotalEpisodios}");
+    }
+}
